f32fec763268dca83c1365087ac8f53afd9a0125
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2021-07-03 17:41:31 +0900

    PJ生成

---
+++ java-spring/src/main/java/com/kugutsu/wordTest/WordTestApplication.java
@@ -0,0 +1,13 @@
+package com.kugutsu.wordTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordTestApplication.class, args);
+	}
+
+}
