e436520534d37365e07e56336cfb7a503848fcde
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2021-12-25 17:14:49 +0900

    addもれ

---
+++ java-spring/src/main/java/com/kugutsu/wordTest/domain/model/exam/Exam.java
@@ -0,0 +1,10 @@
+package com.kugutsu.wordTest.domain.model.exam;
+
+import com.kugutsu.wordTest.domain.model.problem.Problem;
+import java.util.List;
+
+
+public class Exam {
+
+    List<Problem> problems;
+}
