cfe8ddca1038b4be69f7a05b855a052063a7ec2b
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2022-01-09 16:26:09 +0900

    不要コード削除

--- java-spring/src/main/java/com/kugutsu/wordTest/application/service/exam/ExamService.java
+++ java-spring/src/main/java/com/kugutsu/wordTest/application/service/exam/ExamService.java
@@ -2,7 +2,6 @@ package com.kugutsu.wordTest.application.service.exam;
 
 import com.kugutsu.wordTest.domain.model.exam.Exam;
 import com.kugutsu.wordTest.domain.model.exam.Mondai;
-import com.kugutsu.wordTest.application.service.vocabularyBook.VocabularyBookRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,12 +9,6 @@ import java.util.List;
 
 @Service
 public class ExamService {
-
-    private final VocabularyBookRepository vBookRepository;
-
-    public ExamService(VocabularyBookRepository vBookRepository) {
-        this.vBookRepository = vBookRepository;
-    }
 
     /*
      * 問題のリストを作成する
